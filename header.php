c3e3a4f19f9002235955e6734a7ec3bd9a3d15a5
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-04 15:42:17 -0300

    Fixed logo anchor 3

--- header.php
+++ header.php
@@ -54,6 +54,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 						the_custom_logo();
 					} ?><!-- end custom logo -->
 
+				<script>
+					jQuery('a.navbar-brand.custom-logo-link').attr("href", "#")
+				</script>
+
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
