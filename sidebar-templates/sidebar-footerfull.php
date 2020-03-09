719ad4bedf2d40962e978d9b4bfc7b5f4143ad6f
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-09 13:24:58 -0300

    fixed footer logo

--- sidebar-templates/sidebar-footerfull.php
+++ sidebar-templates/sidebar-footerfull.php
@@ -23,8 +23,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<div class="row">
             
-            <div class="col-lg-8 offset-lg-2">
-				<?php echo wp_get_attachment_image( "325", "full", "", array( "class" => "img-fluid mx-auto d-block" ) );  ?>
+            <div class="col-lg-7 offset-lg-2">
+				<?php echo wp_get_attachment_image( "325", "full", "", array( "class" => "img-fluid mx-auto d-block mb-4" ) );  ?>
             </div>
 
 				<?php dynamic_sidebar( 'footerfull' ); ?>
